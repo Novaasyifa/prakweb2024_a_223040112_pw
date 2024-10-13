a52c2afa7b40174d310957ed8994ff825dc0d231
Novaasyifa <126181389+Novaasyifa@users.noreply.github.com>
2024-10-13 11:36:35 -0700

    Add files via upload

---
+++ mvc/app/models/User_model.php
@@ -0,0 +1,12 @@
+
+<?php
+
+class User_model
+{
+  private $nama = 'Nova Asyifa';
+
+  public function getUser()
+  {
+    return $this->nama;
+  }
+}
---
+++ mvc/app/views/home/index.php
@@ -0,0 +1,3 @@
+
+<h1>Selamat datang di website saya</h1>
+  <h1>Halo, nama saya <?= $data ['nama']; ?></h1>
